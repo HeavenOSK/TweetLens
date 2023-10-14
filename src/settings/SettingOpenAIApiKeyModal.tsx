1b25a73e954341b67d549e82f0131dd041298046
HeavenOSK <heavenosk@gmail.com>
2023-10-14 21:21:58 +0900

    input のスタイリング

--- src/settings/SettingOpenAIApiKeyModal.tsx
+++ src/settings/SettingOpenAIApiKeyModal.tsx
@@ -10,27 +10,31 @@ type Props = Pick<UseModal, "isOpen" | "close">
 const SettingOpenAIApiKeyModal = ({ isOpen, close }: Props) => {
   return (
     <BaseDialog isOpen={isOpen} close={close}>
-      <div className="sm:flex sm:items-start">
-        <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10"></div>
-        <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-          <Dialog.Title
-            as="h3"
-            className="text-base font-semibold leading-6 text-gray-900">
-            Deactivate account
-          </Dialog.Title>
-          <div className="mt-2">
-            <p className="text-sm text-gray-500">
-              Are you sure you want to deactivate your account? All of your data
-              will be permanently removed from our servers forever. This action
-              cannot be undone.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div className="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
-        <ConfirmButton label="Confirm" onClick={() => {}} />
+      <div className="flex flex-col">
+        <Dialog.Title
+          as="h3"
+          className="text-base font-semibold leading-6 text-gray-900">
+          OpenAI API Key
+        </Dialog.Title>
+        <p className="text-sm text-gray-500 mt-1">
+          解説に使用するOpenAI API Keyを設定してください。
+          <a
+            href="https://platform.openai.com/account/api-keys"
+            className="font-medium text-indigo-700 underline hover:text-indigo-500"
+            target="_blank">
+            API keys(OpenAI)
+          </a>
+          で取得できます。
+        </p>
 
-        <CancelButton onClick={close} />
+        <div className="flex mt-2">
+          <input
+            type="password"
+            className="block flex-1 px-3 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2  focus:ring-indigo-300 focus:ring-inset sm:text-sm sm:leading-6 focus:outline-none"
+            placeholder="sk-**************"
+          />
+          <ConfirmButton label="設定" onClick={() => {}} />
+        </div>
       </div>
     </BaseDialog>
   )
