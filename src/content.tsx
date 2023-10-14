9c685af6826648179273cdd5126bebe0fc8595ed
HeavenOSK <heavenosk@gmail.com>
2023-10-14 10:54:15 +0900

    update style

--- src/content.tsx
+++ src/content.tsx
@@ -29,7 +29,7 @@ const CustomButton = () => {
   return (
     <div className="mt-2 w-full">
       {result !== null ? (
-        <div className="p-3 min-h-[44px] w-full bg-white text-base font-semibold text-gray-500 tracking-wide border rounded-md drop-shadow-sm leading-6">
+        <div className="p-3 min-h-[44px] w-full bg-white text-base text-gray-600 tracking-wide border rounded-md drop-shadow-sm leading-6">
           {result.map((word, index) => {
             return <span key={index}>{word}</span>
           })}
